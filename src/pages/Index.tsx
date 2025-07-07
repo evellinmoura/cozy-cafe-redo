fb3d8757c8638327a6d9c73e77c98563e7c31dfb
Joaquim Ribeiro <joaquimgamer23@gmail.com>
2025-07-07 00:41:34 -0300

    changed visuals to match figma

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -2,10 +2,11 @@ import { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
-import { ShoppingCart, User, History, LogOut, ChefHat } from "lucide-react";
+import { ShoppingCart, User, History, CircleUserRound, ChevronDown, LogOut, ChefHat } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 import { DrinkModal } from "@/components/DrinkModal";
 import { Cart } from "@/components/Cart";
+import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuItem } from "@/components/ui/dropdown-menu";
 import { set } from "date-fns";
 
 interface Drink {
@@ -119,76 +120,81 @@ const Index = () => {
   return (
     <div className="min-h-screen bg-[#fff9f3]">
       {/* Header */}
-      <header className="bg-white shadow-sm border-b">
-        <div className="max-w-6xl mx-auto px-4 py-4 flex justify-between items-center">
+      <header className="bg-[#f8e0b3] shadow-md">
+        <div className="max-w-6xl mx-auto px-4 py-2 flex justify-between items-center">
           {/*<div className="flex items-center gap-4">
             <h1 className="text-2xl font-bold text-orange-800">☕ Terra&Café</h1>
           </div>*/}
           <div className="logo-colorido flex items-center gap-3">
             <img src="public\terracafe_colorido.svg"  width="50" height="50"></img>
-            <h1 className="text-2xl font-bold text-orange-800">Terra&Café</h1>
+            <h1 className="text-2xl font-bold font-serif text-[#754416]">Terra&Café</h1>
           </div>
-          <div className="flex items-center gap-4">
-            {user ? (
-              <div className="flex items-center gap-4">
-                <Button
-                  variant="outline"
-                  onClick={() => navigate("/history")}
-                >
-                  <History className="h-4 w-4 mr-2" />
-                  Histórico
-                </Button>
-                <Button
-                  variant="outline"
-                  onClick={handleLogout}
-                >
-                  Sair
-                </Button>
-              </div>
-            ) : (
-              <div className="flex gap-2">
-                <Button
-                  variant="outline"
-                  onClick={() => navigate("/login")}
-                >
-                  Entrar
-                </Button>
-              </div>
-            )}
+          <div className="flex items-center gap-2">
             <Button
               variant="outline"
               onClick={() => setShowCart(true)}
-              className="relative"
+              className="hover:bg-[#e2ce87] relative rounded-full border-transparent bg-transparent"
             >
-              <ShoppingCart className="h-5 w-5" />
+              <ShoppingCart className="h-5 w-5 text-[#754416]" />
               {getTotalItems() > 0 && (
                 <Badge className="absolute -top-2 -right-2 h-5 w-5 rounded-full p-0 flex items-center justify-center bg-orange-500">
                   {getTotalItems()}
                 </Badge>
               )}
             </Button>
+            {user ? (
+              <div className="flex items-center gap-4 text-[#754416]">
+                <DropdownMenu>
+                  <DropdownMenuTrigger>
+                    <Button className="hover:bg-[#e2ce87] rounded-full bg-[#d7dfaf] text-[#754416]">
+                      <User className="h-4 w-4 mr-2" />
+                      {user.name}
+                      <ChevronDown className="ml-2 h-4 w-4" />
+                    </Button>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent align="end" className="w-48 bg-[#d7dfaf] text-[#754416] rounded-sm">
+                    <DropdownMenuItem className="hover:bg-[#e2ce87]" onClick={() => navigate("/history")}>
+                      <History className="h-4 w-4 mr-2" />
+                      Histórico
+                    </DropdownMenuItem>
+                    <DropdownMenuItem className="hover:bg-[#e2ce87]" onClick={() => handleLogout()}>
+                      <LogOut className="h-4 w-4 mr-2" />
+                      Sair
+                    </DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              </div>
+            ) : (
+              <div className="flex gap-2 text-[#754416]">
+                <Button
+                  className="rounded-full bg-[#d7dfaf]"
+                  variant="outline"
+                  onClick={() => navigate("/login")}
+                >
+                  <CircleUserRound className="h-4 w-4 mr-2" />
+                  Entrar
+                </Button>
+              </div>
+            )}
           </div>
         </div>
       </header>
 
       {/* Hero Section */}
       <section className="max-w-6xl mx-auto px-4 py-8">
-  <div className="relative">
-    <img 
-      src="/public/a-happy-black-young-woman-on-a-coffee-shop 2.png" 
-      alt="Imagem de café" 
-      className="w-full h-auto"
-    />
-    <div className="absolute bottom-0 left-0 p-4 text-left text-white">
-      <h2 className="text-3xl font-bold text-orange-800 mb-2">
-        Seu café, no seu tempo
-      </h2>
-      <p className="text-orange-700 text-lg">
-        do seu jeitinho 
-      </p>
-    </div>
-  </div>
-</section>
+        <div className="relative">
+          <img 
+            src="/public/a-happy-black-young-woman-on-a-coffee-shop 2.png" 
+            alt="Imagem de café" 
+            className="w-full h-auto"
+          />
+          <div className="bg-[#fff9f3] m-6 rounded-lg absolute bottom-0 left-0 p-3 text-left text-[#754416]">
+            <h2 className="text-3xl font-bold font-serif">
+              Seu café, no seu tempo do seu jeitinho 💛
+            </h2>
+          </div>
+        </div>
+      </section>
 
 
 
@@ -226,6 +232,36 @@ const Index = () => {
           ))}
         </div>
       </section>
+
+      <footer className="bg-[#412a2b]">
+        <div className="max-w-6xl mx-auto px-5 py-8 flex justify-evenly gap-6 items-center">
+          <div className="max-w-xs">
+            <p className="text-[#f8e0b3]">
+              TERRA&CAFÉ
+            </p>
+            <h1 className="text-[#ff751f] text-2xl font-bold font-serif my-4">
+              Seu café, no seu tempo do seu jeitinho
+            </h1>
+            <p className="text-[#776c59]">
+              Todos os direitos reservados © 2025
+            </p>
+          </div>
+          <div className="justify-items-start max-w-xs">
+            <p className="text-[#ff751f] my-2">
+              Sobre Nós
+            </p>
+            <p className="text-[#f8e0b3]">
+              Conheça mais sobre a nossa história e missão.
+            </p>
+            <p className="text-[#f8e0b3]">
+              Contatos
+            </p>
+            <p className="text-[#f8e0b3]">
+              Endereços
+            </p>
+          </div>
+        </div>
+      </footer>
 
       {/* Modals */}
       {selectedDrink && (
